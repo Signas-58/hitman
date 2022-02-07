81cf2e748859992d18aeea1eaa7b320c6fb56ae2
bpking7 <92138643+bpking7@users.noreply.github.com>
2022-02-07 22:52:19 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
 	): Promise<void> => {
 		const user = M.sender.jid;
 		const beckylynch =
-			"assets/a.mp4";
+			"assets/Untitled (1).mp4";
 		if (!parsedArgs.joined) {
 			const commands = this.handler.commands.keys();
 			const categories: { [key: string]: ICommand[] } = {};
