ef06d994f0354f08e264f38be0b9ee200ba06a46
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2022-02-12 10:27:31 +0200

    Create hitman.ts

---
+++ src/commands/Bots/hitman.ts
@@ -0,0 +1,22 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'hitman',
+            description: 'Displays the info',
+            category: 'bots',
+            usage: `${client.config.prefix}void`,
+            baseXp: 100
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        return void M.reply(
+            `👾 *Void* 👾\n\n👿 *Description:* A fully Object Oriented WhatsApp bot built with TypeScript\n\n🌐 *URL:* https://github.com/Dkhitman3/hitman47 \n`
+        ).catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
+    }
+}
