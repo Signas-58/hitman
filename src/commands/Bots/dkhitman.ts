9630246e6dde40bbc6d98f8883d0dfc9a1b47295
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2022-02-14 18:38:23 +0200

    Update dkhitman.ts

--- src/commands/Bots/dkhitman.ts
+++ src/commands/Bots/dkhitman.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'DKHITMAN',
+            command: '𝐡𝐢𝐭𝐦𝐚𝐧',
             description: 'Displays the info',
             category: 'bots',
             usage: `${client.config.prefix}void`,
