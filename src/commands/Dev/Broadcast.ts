2466d43ab402632bd984be3a4eb1012db858a3cb
Dkhitman3 <98841613+Dkhitman3@users.noreply.github.com>
2022-02-18 22:58:02 +0200

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { MessageType } from "@adiwajshing/baileys";
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
 import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
@@ -28,15 +28,16 @@ export default class Command extends BaseCommand {
 		if (!joined)
 			return void (await M.reply(`Please provide the Broadcast Message.`));
 		const term = joined.trim();
-		const images = [
-			"https://wallpapercave.com/w/wp10318653.jpg",
-			"https://wallpapercave.com/w/wp7111330.jpg",
-			"https://wallpapercave.com/w/wp3281482.jpg",
-			"https://wallpapercave.com/w/wp10318669.jpg",
-			"https://wallpapercave.com/w/wp10318667.jpg",
-			"https://wallpapercave.com/w/wp10318653.jpg",
+		const gifs = [
+			"https://media.tenor.com/videos/b5bb295fb219e5cd12cb74d29eaa079c/mp4",
+			"https://media.tenor.com/videos/cb603deb19eefdb7feee3aa7aa1aa7b6/mp4",
+			"https://media.tenor.com/videos/31edd50dd5096731be4690a67142a1aa/mp4",
+			"https://media.tenor.com/videos/8fb551bffa77b6f6d875eb0a0a5e8fa9/mp4",
+			"https://media.tenor.com/videos/e09c85e729650f03ca9099663718e38c/mp4",
+			"https://media.tenor.com/videos/9eda38308ee0b60c51962dde63d203c7/mp4",
+			"https://media.tenor.com/videos/f4c3cd17a4348142d254a1f5f206a0d7/mp4",
 		];
-		const selected = images[Math.floor(Math.random() * images.length)];
+		const selected = gifs[Math.floor(Math.random() * gifs.length)];
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		const chats: any = this.client.chats
 			.all()
@@ -45,8 +46,9 @@ export default class Command extends BaseCommand {
 			.map((jids) => (jids.includes("g.us") ? jids : null))
 			.filter((v) => v);
 		for (let i = 0; i < chats.length; i++) {
-			const text = `*☣️「 hitman47 BROADCAST 」☣️*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
-			this.client.sendMessage(chats[i], { url: selected }, MessageType.image, {
+			const text = `*👾「 hitman47 BROADCAST 」👾*\n\n${term}\n\n Regards ~ *${M.sender.username}*`;
+			this.client.sendMessage(chats[i], { url: selected }, MessageType.video, {
+				mimetype: Mimetype.gif,
 				caption: `${text}`,
 				contextInfo: {
 					mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
